de93148e519b38607a2c1f7779ab3cb0c12d8aaf
DMarkoff <dmitri.markoff@gmail.com>
2021-09-29 11:56:17 +0800

    test

--- main.py
+++ main.py
@@ -1055,6 +1055,7 @@ def create_tire_service_order():
                     print(len(res_))
 
                     rand_id = random.randint(1, len(res_))
+                    print(len(res_))
                     worker_id, worker_first_name, worker_last_name, worker_email, worker_phone = res_[rand_id][0], res_[rand_id][1], res_[rand_id][2], res_[rand_id][3], res_[rand_id][4]
 
                     sql_query = """INSERT INTO tire_service_order (user_id, serv_order_date, u_veh_id, worker_id)
