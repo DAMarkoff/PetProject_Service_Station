134f3d0e3ff4034d039fb8c5599a12e24f2f0373
DMarkoff <dmitri.markoff@gmail.com>
2021-10-01 23:49:43 +0800

    test

--- main.py
+++ main.py
@@ -501,7 +501,8 @@ def user_info():
                 res_1 = cursor.fetchall()
                 # cursor.close()
 
-                if res_1[0] is None:
+                empty_result = []
+                if res_1[0] == empty_result:
                     result_tire_service_order_tasks = 'You do not have any tasks in your tire service order.'
                 else:
                     result_tire_service_order_tasks = []
