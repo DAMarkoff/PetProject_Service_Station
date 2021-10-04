91eacbff0749a00e014f4a58513488e3e8e60d64
dmitri.markoff@gmail.com <dmitri.markoff@gmail.com>
2021-10-05 02:16:03 +0800

    Show must go on...

--- defs.py
+++ defs.py
@@ -1,3 +1,8 @@
+import psycopg2
+
+conn = psycopg2.connect(dbname='user_20_db', user='user_20', password='123', host='159.69.151.133', port='5056')
+cursor = conn.cursor()
+
 def user_exist(email):
     if conn:
         sql_query = "SELECT user_id FROM users WHERE email = '{0}'".format(email)
