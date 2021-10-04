d804d056c78ce7023b811b796716aeb27acd8437
dmitri.markoff@gmail.com <dmitri.markoff@gmail.com>
2021-10-04 23:12:26 +0800

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 
 SWAGGER_URL = '/swagger'
-API_URL = '/swagger/swagger_r.json'
+API_URL = '/static/swagger_u.json'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
