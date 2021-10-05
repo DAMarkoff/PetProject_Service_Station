9301fe70dd295c66ef5034690137b5d020f691bb
DAMarkoff <dmitri.markoff@gmail.com>
2021-10-05 13:14:09 +0800

    Add hash_password

--- defs.py
+++ defs.py
@@ -9,6 +9,7 @@ import datetime
 from flask_swagger_ui import get_swaggerui_blueprint
 import bcrypt
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
 conn = psycopg2.connect(dbname='user_20_db', user='user_20', password='123', host='159.69.151.133', port='5056')
 cursor = conn.cursor()
 
