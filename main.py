17d20a505ed0cf452b12db97ef5902a753a9f901
DAMarkoff <dmitri.markoff@gmail.com>
2021-10-18 00:54:25 +0800

    update

--- main.py
+++ main.py
@@ -1237,6 +1237,7 @@ def tire_service_order():
             service_duration = duration_of_service(tire_repair, tire_change, removing_installing_wheels, balancing,
                                                             wheel_alignment, camera_repair, numbers_of_wheels)
             end_time = order_date + service_duration
+            return int(end_time.hour)
             if int(end_time.hour) > 20:
                 abort(400, description='Sorry, we close at 08:00 pm')
 
